82efb92006a489add8298a18b08896f70e94af28
alianido <alienor.anido@ensae-paristech.fr>
2017-03-24 18:49:16 +0100

    Update script4.js

--- script4.js
+++ script4.js
@@ -57,8 +57,9 @@ $(function () {
                        {y: 61422,
                        name:'1989: Chute du mur de Berlin',
                        marker: {
-                         radius: 10,
-                             symbol: 'circle'
+                         radius: 5,
+                             symbol: 'circle', 
+                         color: '#F90303',
                        }
                        }, 
                        {y: 54813, 
