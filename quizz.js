b9a73d88c502376263efbc54912527fe0447fb02
Pregoles <quentin.cavalan@hotmail.fr>
2017-03-28 18:55:50 +0200

    Update quizz.js

--- quizz.js
+++ quizz.js
@@ -179,8 +179,8 @@
          */
      function processQuestion(choice){
             if(quiz[currentquestion]['choices'][choice] == quiz[currentquestion]['correct']){
-                $('.choice').eq(choice).css({'background-color':'#f4cb33'});
-                $('#explanation').html('<strong>Bonne réponse!</strong>' + htmlEncode(quiz[currentquestion]['explanation'])).css({'color':'#f4cb33'});
+                $('.choice').eq(choice).css({'background-color':'#146b01'});
+                $('#explanation').html('<strong>Bonne réponse!</strong>' + htmlEncode(quiz[currentquestion]['explanation'])).css({'color':'#146b01'});
                 score++;
             } else {
                 $('.choice').eq(choice).css({'background-color':'#D92623'});
